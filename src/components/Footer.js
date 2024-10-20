b600528a90a218565b4e1c3a3a321b6db6902405
AshuChoudharystd <146466303+AshuChoudharystd@users.noreply.github.com>
2024-10-20 23:34:09 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,13 +14,13 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon style" style = {{paddingTop:"20px"}}>
-              <a href="https://www.linkedin.com/in/ashu-choudhary-974ab7274?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+              <a href="https://www.linkedin.com/in/naitik-mishra-28a02928a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
                 <img src={navIcon1} alt="" />
               </a>
               <a href="https://github.com/AshuChoudharystd/AshuChoudharystd">
                 <img src={navIcon2} alt="" />
               </a>
-              <a href="https://www.instagram.com/ashuchoudhary0087/profilecard/?igsh=cHRsdDdlNXlwZndr">
+              <a href="https://www.instagram.com/naitikmishra96026?igsh=MWlxbWV3bGw1dG1xZQ==">
                 <img src={navIcon3} alt="" />
               </a>
             </div>
